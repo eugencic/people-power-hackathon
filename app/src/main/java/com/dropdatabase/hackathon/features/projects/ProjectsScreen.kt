5013c747cb44c3fb2226e1c0fdedabc510186c5c
alchetrus <mindrescumihai99@gmail.comm>
2023-11-18 23:17:45 +0200

    [Mobile] add year filter to ProjectsScreen

--- app/src/main/java/com/dropdatabase/hackathon/features/projects/ProjectsScreen.kt
+++ app/src/main/java/com/dropdatabase/hackathon/features/projects/ProjectsScreen.kt
@@ -5,26 +5,40 @@ package com.dropdatabase.hackathon.features.projects
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.dropdatabase.hackathon.common.composeui.components.AppBar
+import com.dropdatabase.hackathon.common.composeui.components.AppFloatingActionButton
 import com.dropdatabase.hackathon.common.composeui.theme.AppTheme
 
 @Composable
@@ -49,6 +63,9 @@ private fun ProjectsScreen(
 ) {
     var selectedTabIndex by remember { mutableStateOf(0) }
     val tabs = listOf("Proiecte de stat", "Proiecte obstesti")
+    var showBottomSheet by remember { mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState()
+    var selectedYear by remember { mutableIntStateOf(0) }
 
     Scaffold(
         topBar = {
@@ -86,6 +103,11 @@ private fun ProjectsScreen(
                     )
                 }
             }
+        },
+        floatingActionButton = {
+            AppFloatingActionButton(onClick = {
+                showBottomSheet = true
+            })
         }
     ) { paddingValues ->
         Surface(
@@ -94,7 +116,62 @@ private fun ProjectsScreen(
                 .fillMaxSize(),
             color = AppTheme.colors.background
         ) {
+            if (showBottomSheet) {
+                BottomSheet(
+                    modifier = modifier,
+                    selectedYear = selectedYear,
+                    sheetState = sheetState,
+                    onDismissRequest = {
+                        showBottomSheet = false
+                    },
+                    onSelectYear = { year -> selectedYear = year })
+            }
+        }
+    }
+}
 
+@Composable
+private fun BottomSheet(
+    modifier: Modifier,
+    sheetState: SheetState,
+    selectedYear: Int,
+    onDismissRequest: () -> Unit,
+    onSelectYear: (selectedYear: Int) -> Unit,
+) {
+
+    val yearList = listOf(2023, 2022, 2021, 2020, 2019)
+
+    ModalBottomSheet(
+        modifier = Modifier.height(150.dp),
+        onDismissRequest = onDismissRequest,
+        sheetState = sheetState
+    ) {
+        LazyRow(
+            modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+        ) {
+            items(yearList) {
+                FilterChip(
+                    onClick = {
+                        onSelectYear(it)
+                    },
+                    label = {
+                        Text(it.toString())
+                    },
+                    selected = selectedYear == it,
+                    leadingIcon = if (selectedYear == it) {
+                        {
+                            Icon(
+                                imageVector = Icons.Filled.Done,
+                                contentDescription = "Done icon",
+                                modifier = Modifier.size(FilterChipDefaults.IconSize)
+                            )
+                        }
+                    } else {
+                        null
+                    },
+                )
+            }
         }
     }
 }
@@ -104,3 +181,4 @@ private fun ProjectsScreen(
 private fun PreviewScreen() {
     ProjectsScreen(onNavigateBack = { /*TODO*/ }, regionName = "Balti")
 }
+
