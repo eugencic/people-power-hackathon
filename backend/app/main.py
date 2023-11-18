4ada64d20862ba6d439f705099ce1c3dae689138
Eugen <72609351+eugencic@users.noreply.github.com>
2023-11-18 17:25:33 +0200

    fix dannics errors

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,17 +19,13 @@ def get_all_records(table_name):
     try:
         conn = psycopg2.connect(**db_params)
         cursor = conn.cursor()
-
         query = sql.SQL("SELECT * FROM {}").format(sql.Identifier(table_name))
         cursor.execute(query)
-
         columns = [desc[0] for desc in cursor.description]
         records = [dict(zip(columns, row)) for row in cursor.fetchall()]
-
         conn.commit()
         cursor.close()
         conn.close()
-
         return records
     except psycopg2.Error as e:
         print("Error connecting to PostgresSQL:", e)
@@ -56,45 +52,35 @@ def save_pdf_to_db(project_id, filename):
     try:
         conn = psycopg2.connect(**db_params)
         cursor = conn.cursor()
-
-        # Update the Projects table with the PDF file name
         query = sql.SQL("UPDATE projects SET pdf_file = %s WHERE project_id = %s")
         cursor.execute(query, (filename, project_id))
-
         conn.commit()
         cursor.close()
         conn.close()
     except psycopg2.Error as e:
-        print("Error connecting to PostgreSQL:", e)
+        print("Error connecting to PostgresSQL:", e)
 
 
-@app.route('api/send_pdf', methods=['GET', 'POST'])
-def upload_file():
+@app.route('/api/send_pdf', methods=['GET', 'POST'])
+def send_pdf():
     if 'file' not in request.files or 'project_id' not in request.form:
         flash('Invalid request')
         return redirect(request.url)
-
     file = request.files['file']
     project_id = request.form['project_id']
-
     if file.filename == '':
         flash('No selected file')
         return redirect(request.url)
-
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
-
-        # Save the PDF file information to the database
         save_pdf_to_db(project_id, filename)
-
         return jsonify({"success": True, "message": "File uploaded and database updated successfully"})
-
     return jsonify({"success": False, "message": "Invalid file format"})
 
 
-@app.route("api/get_pdf/<string:filename>")
+@app.route("/api/get_pdf/<string:filename>")
 def return_pdf(filename):
     return send_file(f"./storage/{filename}")
 
