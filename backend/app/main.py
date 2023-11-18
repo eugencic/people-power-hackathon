b426bb212f4dadcd228405fd13641f57e428a58a
Eugen <72609351+eugencic@users.noreply.github.com>
2023-11-18 16:42:17 +0200

    fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -65,15 +65,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    return "New file uploaded"
 
 
 @app.route("/get_pdf/<string:filename>")
