122190710b49b3659844f534c10d2eabc508b218
Eugen <72609351+eugencic@users.noreply.github.com>
2023-11-18 16:36:59 +0200

    add file uploader

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, flash, request, redirect, send_file, jsonify
 import psycopg2
 from psycopg2 import sql
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -38,6 +40,50 @@ def get_all_records(table_name):
 def get_records(table_name):
     records = get_all_records(table_name)
     return jsonify(records)
+
+
+@app.route("/get_pdf/<string:filename>")
+def return_pdf(filename):
+    return send_file(f"./storage/{filename}")
+
+
+UPLOAD_FOLDER = './storage'
+ALLOWED_EXTENSIONS = {'pdf'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/send_pdf', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+@app.route("/get_pdf/<string:filename>")
+def return_pdf(filename):
+    return send_file(f"./storage/{filename}")
 
 
 if __name__ == '__main__':
