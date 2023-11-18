43d7a24d2c7f4ac5842a9101bbe4e3683ae324de
dan-planable <danpogorevici@planable.io>
2023-11-18 15:20:51 +0200

    add endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,44 @@
-from flask import Flask
+from flask import Flask, jsonify
+import psycopg2
+from psycopg2 import sql
 
-my_app = Flask(__name__)
+app = Flask(__name__)
 
+# Replace these values with your actual PostgreSQL credentials
+db_params = {
+    'dbname': 'database',
+    'user': 'postgres',
+    'password': 'password',
+    'host': 'localhost',
+    'port': '5432'
+}
 
-@my_app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def get_all_records(table_name):
+    try:
+        conn = psycopg2.connect(**db_params)
+        cursor = conn.cursor()
 
+        query = sql.SQL("SELECT * FROM {}").format(sql.Identifier(table_name))
+        cursor.execute(query)
+
+        columns = [desc[0] for desc in cursor.description]
+        records = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        return records
+    except psycopg2.Error as e:
+        print("Error connecting to PostgreSQL:", e)
+        return []
+
+# Parametrized endpoint for fetching all records of a table
+@app.route('/api/<table_name>', methods=['GET'])
+def get_records(table_name):
+    records = get_all_records(table_name)
+    return jsonify(records)
 
 if __name__ == '__main__':
-    my_app.run(port=6666, debug=True)
+    app.run(debug=True)
+
